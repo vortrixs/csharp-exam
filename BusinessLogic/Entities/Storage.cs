dc57f6ca111f305efa0be9c77e5a4b23d8e85bf1
Hans Erik Jepsen <hanserikjepsen@hotmail.com>
2019-11-18 19:57:06 +0100

    Added BL layer

---
+++ BusinessLogic/Entities/Storage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Entities
+{
+	class Storage
+	{
+		public int Create()
+		{
+			return 0;
+		}
+
+		public void Read()
+		{
+			// return data
+		}
+
+		public int Update()
+		{
+			return 0;
+		}
+
+		public bool Delete()
+		{
+			return true;
+		}
+	}
+}
